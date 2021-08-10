8c129fe57d0fb794ae1a96d8b6740add82c7e6f6
returnEdo <fraccaroedo@gmail.com>
2021-08-10 17:27:52 +0200

    More decent controls

--- Controller.cpp
+++ Controller.cpp
@@ -36,38 +36,32 @@ void updateCamera(void)
 
 	Math::vec3 l_deltaPosition(0.0f);
 
-	if (g_inputManager.isDown(GLFW_KEY_RIGHT))	{ l_deltaPosition.x += C_DELTA_POSITION; }
-	if (g_inputManager.isDown(GLFW_KEY_LEFT))	{ l_deltaPosition.x -= C_DELTA_POSITION; }
-	if (g_inputManager.isDown(GLFW_KEY_UP))		{ l_deltaPosition.z -= C_DELTA_POSITION; }
-	if (g_inputManager.isDown(GLFW_KEY_DOWN))	{ l_deltaPosition.z += C_DELTA_POSITION; }
-	if (g_inputManager.isDown(GLFW_KEY_PAGE_UP))	{ l_deltaPosition.y += C_DELTA_POSITION; }
-	if (g_inputManager.isDown(GLFW_KEY_PAGE_DOWN))	{ l_deltaPosition.y -= C_DELTA_POSITION; }
+//	if (g_inputManager.isDown(GLFW_KEY_RIGHT))	
+//	if (g_inputManager.isDown(GLFW_KEY_LEFT))	
+//	if (g_inputManager.isDown(GLFW_KEY_UP))		
+//	if (g_inputManager.isDown(GLFW_KEY_DOWN))	
+//	if (g_inputManager.isDown(GLFW_KEY_PAGE_UP))	
+//	if (g_inputManager.isDown(GLFW_KEY_PAGE_DOWN))	
 
-	if (g_inputManager.isDown(GLFW_KEY_A))		{ s_thetay += C_DELTA_ANGLE; }
-	if (g_inputManager.isDown(GLFW_KEY_D))		{ s_thetay -= C_DELTA_ANGLE; }
-	if (g_inputManager.isDown(GLFW_KEY_W))		{ s_thetax += C_DELTA_ANGLE; }
-	if (g_inputManager.isDown(GLFW_KEY_S))		{ s_thetax -= C_DELTA_ANGLE; }
+	if (g_inputManager.isDown(GLFW_KEY_A))		{ l_deltaPosition.x -= C_DELTA_POSITION; }
+	if (g_inputManager.isDown(GLFW_KEY_D))		{ l_deltaPosition.x += C_DELTA_POSITION; }
+	if (g_inputManager.isDown(GLFW_KEY_W))		{ l_deltaPosition.z -= C_DELTA_POSITION; }
+	if (g_inputManager.isDown(GLFW_KEY_S))		{ l_deltaPosition.z += C_DELTA_POSITION; }
+	if (g_inputManager.isDown(GLFW_KEY_Q))		{ l_deltaPosition.y += C_DELTA_POSITION; }
+	if (g_inputManager.isDown(GLFW_KEY_E))		{ l_deltaPosition.y -= C_DELTA_POSITION; }
 
 	if (g_inputManager.isDown(GLFW_KEY_P))		{ glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); }
 	if (g_inputManager.isDown(GLFW_KEY_L))		{ glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); }
 
-	// Mouse rotation
-	Math::vec2 l_deltaMouse = g_inputManager.mMousePosition - s_previousMousePosition;
-
-	// In cam coordinates
-	Math::vec2 l_this { g_inputManager.mMousePosition.x - g_windowManager.mWidth / 2.0f, -g_inputManager.mMousePosition.y + g_windowManager.mHeight / 2.0f };
-	Math::vec2 l_prev { s_previousMousePosition.x - g_windowManager.mWidth / 2.0f, -s_previousMousePosition.y + g_windowManager.mHeight / 2.0f };
+	// Mouse control
+	float l_mousex = g_inputManager.mMousePosition.x - g_windowManager.mWidth / 2.0f;
+	float l_mousey = -g_inputManager.mMousePosition.y + g_windowManager.mHeight / 2.0f;
 	
-	l_this *= 2.0f * l_view.m_nearPlane * std::atan(l_view.m_fov / 2.0f) / g_windowManager.mWidth;
-	l_prev *= 2.0f * l_view.m_nearPlane * std::atan(l_view.m_fov / 2.0f) / g_windowManager.mWidth;
-
-	Math::vec2 l_deltaC = l_this - l_prev;
-
-	s_thetax -= std::atan(l_deltaC.y / l_view.m_nearPlane)/3.0f;
-	s_thetay += std::atan(l_deltaC.x / l_view.m_nearPlane)/3.0f;
-
-	s_previousMousePosition = g_inputManager.mMousePosition;
-
+	float k = 1.0f / 10000.0f;
+	float delta = 100.0f;
+	
+	s_thetay -= Math::sign<float>(l_mousex) * std::max(0.0f, k * (std::abs(l_mousex) - delta));
+	s_thetax += Math::sign<float>(l_mousey) * std::max(0.0f, k * (std::abs(l_mousey) - delta));
 
 	s_thetax = Math::clamp<float>(s_thetax, -M_PI/2.0f + C_OFFSET_ANGLE, M_PI/2.0f - C_OFFSET_ANGLE);
 
